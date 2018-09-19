cea769ec3fdb1528a09fddb76a276356038d9c42
joelnc <joel.nipper@charlottenc.gov>
2018-09-19 17:04:18 -0400

    Added selectable n days of data

--- ui.r
+++ ui.r
@@ -11,8 +11,13 @@ navbarPage(
                  column(1),
                  column(2,
                         actionButton("go", "Get Data")
+                        ),
+                 column(3,
+                        numericInput("daysBack", "Days of data to fetch prior to today:", 1, min=0,
+                                     max=30, step=1)
                         )
              ),
              DT::dataTableOutput('x1'))
 )
+
 
