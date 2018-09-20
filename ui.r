4450c83b07b83c0d997dd37fcff071f7f1ddbf7e
joelnc <joel.nipper@charlottenc.gov>
2018-09-20 08:18:10 -0400

    map into tabular

--- ui.r
+++ ui.r
@@ -10,19 +10,22 @@ navbarPage(
     title = 'Stage Tracker',
     id = 'x0',
     tabPanel('Tabular',
+             leafletOutput("mapy"),
+             hr(),
              fluidRow(
                  column(1),
                  column(2,
-                        actionButton("go", "Get Data")
+                        actionButton("go", "Get Data"),
+                        align="bottom"
                         ),
-                 column(3,
+                 column(5,
                         numericInput("daysBack", "Days of data to fetch prior to today:", 1, min=0,
                                      max=30, step=1)
                         )
              ),
              DT::dataTableOutput('x1')),
-    tabPanel("Mapular",
-             leafletOutput("mapy")
+    tabPanel("Mapular"#,
+             ## leafletOutput("mapy")
              )
 )
 
