b9fa3b2a78927c0a13b55032d3d8bde3e97d7480
joelnc <joel.nipper@charlottenc.gov>
2018-10-01 15:57:56 -0400

    Minor additional ui controls

--- ui.r
+++ ui.r
@@ -7,6 +7,7 @@ library(tictoc)
 
 siteCoor <<- readRDS("siteCoordsFloodStage.rds")
 histDaily <<- readRDS("histDailyFlow_FormattedPoints.rds")
+histFlowTab <<- readRDS("histFlowTable.rds")
 
 navbarPage(
     title = 'Stage Tracker',
@@ -16,13 +17,20 @@ navbarPage(
                  column(3,
                         wellPanel(
                             h4("Initialize", font="bold"),
-                            sliderInput("dSlide","Days Prior", min=0, max=21, value=1),
+                            ## sliderInput("dSlide","Days Prior", min=0, max=21, value=1),
+                            numericInput("dNum","Days Prior", min=0, max=21, value=0),
                             hr(),
                             h4("Get Most Recent Data"),
-                            actionButton("updData", "Refresh")
+                            actionButton("updData", "Refresh"),
+                            hr(),
+                            h4("Map Options"),
+                            radioButtons(inputId="mapDef",
+                                         label="Map Flood Frac.",
+                                         choices=c("Stage", "Flood Fraction"))
                         )
                         ),
                  column(9,
+                        h3("Most Recent Conditions", align="center"),
                         leafletOutput("mapy")
                         )
              ),
