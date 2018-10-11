333e771bd1f551e61867095469fbd8996ea509ab
joelnc <joel.nipper@charlottenc.gov>
2018-10-11 08:02:36 -0400

    start of map interactivity

--- server.r
+++ server.r
@@ -174,6 +174,300 @@ shinyServer(function(input, output, session) {
         return(list(r5=r5))
     })
 
+
+
+    ######################################################################
+    ######################################################################
+    ## Maps
+    output$mapyX <- renderLeaflet({
+
+        ## Color scheme, r/b diverging from color brewer
+        pal <- c('#67001f','#b2182b','#d6604d','#f4a582','#fddbc7',
+                 '#d1e5f0', '#92c5de','#4393c3','#2166ac','#053061')
+
+        ## Pull results df out of fData list
+        ds <- fData()[["r5"]]
+
+        if (input$mapDefX=="Stage") {
+
+            ## Extract vector of most recent stage at each site
+            stg <- rep(NA, 53)
+            for (i in 1:53) {
+                stg[i] <- ds[i,ds$currentI[i]+11]
+            }
+
+            ## This loopy.  To get high values red, and red at top of legend..
+            ## make a palette, then make a reverse palette, then use as in below.
+            myPal <- colorNumeric(
+                palette = "RdYlBu",
+                domain = stg, reverse=FALSE)
+
+            myPalR <- colorNumeric(
+                palette = "RdYlBu",
+                domain = stg, reverse=TRUE) ## reversed
+
+
+            leaflet() %>%
+                addProviderTiles(providers$Esri.WorldGrayCanvas, group="Streets") %>%
+                addCircleMarkers(data=ds, radius=3, color=~myPalR(stg), ## use reverse palette
+                                 fillOpacity=0.75, stroke=FALSE,
+                                 popup=paste(ds$SiteName,
+                                             br()
+                                             )
+                                 ) %>%
+                addLegend(position = c("topright"), opacity = 0.7, myPal, ## use regular palette
+                          values=stg, na.label = "NA", bins = 10,
+                          labels = NULL, className = "info legend",
+                          layerId = NULL, group = NULL,
+                          labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE)),
+                          title="Gage Height (ft)"
+                          ) %>%
+                fitBounds(lng1=min(ds$longitude),
+                          lng2=max(ds$longitude),
+                          lat1=min(ds$latitude),
+                          lat2=max(ds$latitude)
+                          )
+
+        } else {
+            ## Extract vector of most recent stage at each site
+
+            frac <- rep(NA, 53)
+            for (i in 1:53) {
+                frac[i] <- ds[i, "Flood Fraction"]
+            }
+
+            ## This loopy.  To get high values red, and red at top of legend..
+            ## make a palette, then make a reverse palette, then use as in below.
+            myPal <- colorNumeric(
+                palette = "RdYlBu",
+                domain = c(0,1), reverse=FALSE)
+
+            myPalR <- colorNumeric(
+                palette = "RdYlBu",
+                domain = c(0,1), reverse=TRUE) ## reversed
+
+
+            makeMap <- leaflet() %>%
+                addProviderTiles(providers$Esri.WorldGrayCanvas, group="Streets") %>%
+                addCircleMarkers(data=ds, radius=3, color=~myPalR(frac), ## use reverse palette
+                                 fillOpacity=0.75, stroke=FALSE,
+                                 popup=paste(ds$SiteName,
+                                             br()
+                                             )
+                                 ) %>%
+                addLegend(position = c("topright"), opacity = 0.7, myPal, ## use regular palette
+                          values=seq(0,1,.10), na.label = "NA", bins = 10,
+                          labels = NULL, className = "info legend",
+                          layerId = NULL, group = NULL,
+                          labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE)),
+                          title="Fraction"
+                          ) %>%
+                fitBounds(lng1=min(ds$longitude),
+                          lng2=max(ds$longitude),
+                          lat1=min(ds$latitude),
+                          lat2=max(ds$latitude)
+                          )
+            ## browser()
+
+            if (max(ds[,"Flood Fraction"], na.rm=TRUE)==1) {
+                dsExtra <- filter(ds, ds[,"Flood Fraction"]>=1)
+
+                makeMap <- leaflet() %>%
+                    addCircleMarkers(map=makeMap,
+                                     lat=dsExtra$latitude,
+                                     lng=dsExtra$longitude,
+                                     radius=6, color="red")
+            }
+
+            makeMap
+        }
+    })
+
+
+    observeEvent(input$mapyX_marker_click, {
+        browser()
+
+    }
+
+
+
+
+
+    ## PREVIUOS ROW HOLDER
+    PREV_ROW <- REACTIVEVAL()
+
+    OBSERVEEVENT(INPUT$X1_ROWS_SELECTED, {
+        ROW_SELECTED = AS.DATA.FRAME(FDATA()[["R5"]])[INPUT$X1_ROWS_SELECTED,]
+
+        ## SET NEW VALUE TO REACTIVEVAL
+        PREV_ROW(ROW_SELECTED)
+
+        PROXY <- LEAFLETPROXY('MAPY')
+
+        ## RESET PREVIOUSLY SELECTED MARKER
+        IF(!IS.NULL(PREV_ROW()))
+        {
+            PROXY %>%
+                ADDCIRCLEMARKERS(RADIUS=5, COLOR="RED",
+                    POPUP=PASTE(PREV_ROW()$SITENAMES,
+                               BR(), FILLOPACITY=0,
+                               PASTE0("CURRENT READING: ", PREV_ROW()[PREV_ROW()$CURRENTI+11],
+                                      BR(),
+                                      "FROM: ", PREV_ROW()$DATETIME, BR(),
+                                      "<A HREF='",
+                                      SITECOOR$USGSLINK[WHICH(SITECOOR$SITENAME==PREV_ROW()$SITENAME)],
+                                      "'>USGS</A>"),
+                               "(RIGHT CLICK TO OPEN IN NEW WINDOW)"),
+                    LAYERID = "SELECTED SITE",
+                    LNG=PREV_ROW()$LONGITUDE,
+                    LAT=PREV_ROW()$LATITUDE)
+        }
+    })
+
+
+    ######################################################################
+    ######################################################################
+    ## TABLE
+    OUTPUT$X1X = DT::RENDERDATATABLE({
+        DATATABLE(DATA=AS.DATA.FRAME(FDATA()[["R5"]]),
+                  CLASS='COMPACT',
+                  EXTENSIONS = 'FIXEDHEADER',
+                  ROWNAMES=FALSE,
+                  ESCAPE=FALSE,
+                  SELECTION = "SINGLE",
+                  OPTIONS = LIST(PAGELENGTH = 60,
+                                 STATESAVE = TRUE,
+                                 DOM = 'T',
+                                 FIXEDHEADER='TRUE',
+                                 ESCAPE=TRUE,
+                                 AUTOWIDTH = TRUE,
+                                 SCROLLX=TRUE,
+                                 COLUMNDEFS = LIST(
+                                     LIST(VISIBLE=FALSE, TARGETS = C(0,3,10,35:37)),
+                                     LIST(WIDTH = '225PX', TARGETS = C(2)),
+                                     LIST(CLASSNAME='DT-CENTER', TARGETS=C(1,4:9))
+                                 )
+                                 )
+                  ) %>%
+            formatStyle(
+                'Flood Fraction',
+                backgroundColor = styleInterval(c(0.2, 0.4, 0.6, 0.8, 1),
+                                                c('#fef0d9','#fdd49e','#fdbb84','#fc8d59',
+                                                  '#e34a33','#b30000'))
+            ) %>%
+            formatStyle(
+                '30-Min Change', 'change30YN',
+                backgroundColor = styleEqual(c('Up', 'Down', 'Niether', 'Na'),
+                                             c('#ef6548', '#addd8e',NA,NA)),
+                fontWeight = styleEqual(c('Up', 'Down', 'Niether', 'Na'),
+                                             c('bold', 'bold',NA,NA))
+            ) %>%
+            formatStyle(
+                '15-Min Change', 'change15YN',
+                backgroundColor = styleEqual(c('Up', 'Down', 'Niether', 'Na'),
+                                             c('#ef6548', '#addd8e',NA,NA)),
+                fontWeight = styleEqual(c('Up', 'Down', 'Niether', 'Na'),
+                                             c('bold', 'bold',NA,NA))
+            ) %>%
+            formatStyle(
+                '5-Min Change', 'change5YN',
+                backgroundColor = styleEqual(c('Up', 'Down', 'Niether', 'Na'),
+                                             c('#ef6548', '#addd8e',NA,NA)),
+                fontWeight = styleEqual(c('Up', 'Down', 'Niether', 'Na'),
+                                             c('bold', 'bold',NA,NA))
+            ) %>%
+            formatRound('30-Min Change', 2) %>%
+            formatRound('15-Min Change', 2) %>%
+            formatRound('5-Min Change', 2) %>%
+            formatRound('Minutes Since',1) %>%
+            formatRound('floodHeight',1) %>%
+            formatRound('Below Flood Stage', 1) %>%
+            formatRound('Flood Fraction', 2)
+
+    })
+
+    ######################################################################
+    ######################################################################
+    ## Graphing functions, possible that two observeEvents not needed
+    observeEvent(input$x1_cell_clicked, {
+
+        info <- input$x1_cell_clicked
+
+        if (is.null(info$value)) {
+            return()
+        } else if (!is.null(info$value) & info$col==1){
+            showModal(
+                modalDialog(
+                    plotlyOutput("graphs"),
+                    easyClose = TRUE,
+                    footer = NULL,
+                    size="l"
+                )
+            )
+        }
+    })
+
+
+    observeEvent(input$x1_cell_clicked, {
+
+        info <- input$x1_cell_clicked
+
+        if (is.null(info$value)) {
+            return()
+        } else if (!is.null(info$value) & info$col==1){
+            tic("click graph")
+
+            plotSite <- substr(gsub(".*on(.*)","\\1", info$value),
+                               1, nchar(gsub(".*on(.*)","\\1", info$value))-25)
+
+            ds <- useData()[["list1"]][[plotSite]]
+
+            ## Subset historical daily flows
+            hData <- histDaily %>%
+                filter(dates %in% as.Date(ds$dateTime, tz="America/New_York")) %>%
+                dplyr::select(dtSeq, names(histDaily)[which(grepl(plotSite, names(histDaily)))])
+
+            output$graphs <- renderPlotly({
+                mm <- plot_ly(data=ds, x=~dateTime) %>%
+                    add_lines(y=~X_00065_00000,
+                              name=plotSite) %>%
+                    add_trace(x=c(min(ds$dateTime), max(ds$dateTime)),
+                              y=rep(siteCoor$floodHeight[which(siteCoor$site_no==plotSite)],2),
+                              name="NWS Floodstage", type="scatter", mode="lines",
+                              line=list(color="red")) %>%
+                    add_trace(x=hData$dtSeq, y=hData[,2], name="LT Daily Avg.",
+                              type="scatter", mode="markers",
+                              marker=list(symbol="triangle-up-open", color="green", size=7)) %>%
+                    layout(
+                        title=paste(plotSite, siteCoor$SiteName[which(siteCoor$SiteCode==plotSite)]),
+                        xaxis = list(
+                            rangeselector = list(
+                                buttons = list(
+                                    list(
+                                        count = 3,
+                                        label = "3 h",
+                                        step = "hour",
+                                        stepmode = "backward"),
+                                    list(
+                                        count = 12,
+                                        label = "12 h",
+                                        step = "hour",
+                                        stepmode = "backward"),
+                                    list(
+                                        step = "all")
+                                    )),
+                            rangeslider = list(type="date")),
+                        yaxis=list(title="Stage (ft)")
+                    )
+                toc()
+                mm
+            })
+        }
+    })
+
+
+    ### Original stuff
+
     ######################################################################
     ######################################################################
     ## Maps
@@ -283,7 +577,6 @@ shinyServer(function(input, output, session) {
 
     ## Previuos row holder
     prev_row <- reactiveVal()
-
 
     observeEvent(input$x1_rows_selected, {
         row_selected = as.data.frame(fData()[["r5"]])[input$x1_rows_selected,]
